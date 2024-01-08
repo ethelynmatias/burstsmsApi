b8d3030100688ecef9f03acf82ca5dc4c506b252
ethelynmatias <ethelyn.matias2015@gmail.com>
2024-01-08 09:41:32 +0800

    update api

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This is just code that I compile to help other developers. Burst SMS API Documentation link: https://burstsms.com/sms-api
+This is just code that I compile to help other developers and just to simplify the codes. 
+Burst SMS API Documentation link: https://burstsms.com/sms-api
--- connect-with-sms-api.php
+++ connect-with-sms-api.php
@@ -1,10 +1,35 @@
 <?php 
-    //connect the burst sms api
+    //CONNECT TO BURST API
+    //SET UP THE KEYS
     $key = '[ENTER KEY HERE]';
     $secret = '[ENTER SECRET HERE]';
 
+    //CREATE SMS BODY
     $body = "This is a simple sms send via burs sms api";
+
+    //ENTER MOBILE NUMBER WHERE YOU WANT TO SEND THE SMS
     $mobile = "[ENTER MOBILE NUMBER HERE]";
 
-    $url ='https://api.transmitsms.com/send-sms.json?message='. $body.'&reply_callback=https://www.myserver.com/processreply.php?myparameter=myvalue&to='. $mobile.'&from=[ENTER NUMBER HERE]';
+    //SEND SMS FUNCTION
+    $url ='https://api.transmitsms.com/send-sms.json?message='. $body.'&reply_callback=https://www.myserver.com/processreply.php?myparameter=myvalue&to='. $mobile.'&from=[ENTER FROM NUMBER HERE]';
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            "Content-Type: application/json",
+            "Authorization: Basic ".base64_encode($key.":".$secret)
+        ),
+    ));
+
+    $response = curl_exec($curl);
+    $validate = json_decode($response);
 ?>
